76abffff3f3d3adb6c18064cff20180c66599aaf
MahaPRANAV <MAHALAKSHMI.RAMU@GMAIL.COM>
2017-02-22 17:54:30 +0530

    Add files via upload
    Java Session One Assignment 2

---
+++ JavaOneAssignmentTwo.java
@@ -0,0 +1,16 @@
+
+public class JavaOneAssignmentTwo {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+
+		System.out.println("Value of A "+ a) ;
+		System.out.println("Value of B "+ b) ;
+		System.out.println("Value of C "+ c) ;
+		System.out.println("Value of D "+ d) ;
+		System.out.println("Value of E "+ e) ;
+	}
+}
